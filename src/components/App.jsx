c29fb4a12c58f817ce6ad967c1f2fc0fbd7d4731
Yuliia <lupin773311asd@gmail.com>
2025-03-15 21:29:47 +0000

    978o

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css"; // Подключаем стили для компонента
 
+// Импортируем изображение
+import backgroundImage from '../assets/images/mochi.webp';  // Важно указать правильный путь
+
 function App() {
   return (
-    <header className="header">  {/* Применяем класс header для фона */}
-      {/* Навигация, импортируемая из Navigation.jsx */}
+    <header
+      className="header"
+      style={{ backgroundImage: `url(${backgroundImage})` }}  // Используем импортированное изображение
+    >
+      {/* Навигация и остальной контент */}
     </header>
   );
 }
 
 export default App;
+
+
