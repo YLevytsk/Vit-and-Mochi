8c860d512a63d6d07cb1ba54f46a7182b9d51b4c
Yuliia <lupin773311asd@gmail.com>
2025-03-16 18:54:31 +0000

    fsf

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css"; // Глобальные стили
-import App from "./components/App"; // Главный компонент
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/App';
+import Navigation from './components/Navigation'; // Импортируем компонент Navigation
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Navigation /> {/* Добавляем компонент Navigation */}
+    <App /> {/* Оставляем App, если нужен */}
+  </StrictMode>
 );
-
