c9c93d00102c453b46e42d951e817dff0ce49f56
Yuliia <lupin773311asd@gmail.com>
2025-03-16 18:41:31 +0000

    авп

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr"; // Импортируем плагин для SVG
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Vit-and-Mochi/", // Укажи имя репозитория
-  plugins: [react()],
+  base: "/Vit-and-Mochi/", // Добавляем имя репозитория для GitHub Pages
+  plugins: [
+    react(),
+    svgr() // Добавляем плагин svgr
+  ],
   build: {
-    outDir: "dist",
     sourcemap: true,
-  },
+  }
 });
